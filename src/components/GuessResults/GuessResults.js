c5e98053fbe60cd5c4e71ed7863537f88c3fbd2e
Lisa Chudesnova <lizzuka@gmail.com>
2026-01-27 16:40:10 +0100

    Add guess slots

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Guess from '../Guess';
+import { range } from '../../utils';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants'
 
 function GuessResults({ guessList }) {
   return (
     <div className='guess-results'>
-      {guessList.map((guess) => (
-        <p className='guess' key={Math.random()}>{guess}</p>
+      {range(NUM_OF_GUESSES_ALLOWED).map((rowIndex) => (
+        <Guess key={rowIndex} guess={guessList[rowIndex] || ''} />
       ))}
     </div>
   );
